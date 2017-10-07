b32895fdd916c7a0157b30ddfeb789cd01e944ef
acezuo <tozuoc@aliyun.com>
2017-10-07 22:44:36 +0800

    add default WebsocketHandshakeHandler

---
+++ src/main/java/com/zuoc/websocket/protocol/handshake/DefaultWebsocketHandshakeHandler.java
@@ -0,0 +1,14 @@
+package com.zuoc.websocket.protocol.handshake;
+
+import java.util.Map;
+
+/**
+ * Created by zuoc on 2017/10/7.
+ */
+public class DefaultWebsocketHandshakeHandler implements WebsocketHandshakeHandler {
+
+    @Override
+    public Map<String, Object> doHandshake(WebsocketHandshake websocketHandshake) throws WebsocketHandshakeFailureException {
+        return null;
+    }
+}
